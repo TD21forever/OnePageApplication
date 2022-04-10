083e1cec63cbf23d40d8d6055102a389aa1e392a
TD21forever <435212619@qq.com>
2022-04-11 01:09:14 +0800

    Initial Commit

---
+++ OnePageApplication/OnePageApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnePageApplicationApp.swift
+//  OnePageApplication
+//
+//  Created by T D on 2022/4/11.
+//
+
+import SwiftUI
+
+@main
+struct OnePageApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
